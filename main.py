449be9623b04f2a74d09b26fdbd9b137980644e2
menezesalexandre_dev <alexandremmnzssilva2509@gmail.com>
2023-12-05 11:50:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -25,15 +25,18 @@ def form_cidade():
     text = request.form['input_cidade'] + ', BR'
     processed_text = text.capitalize()
     cidade_tempo = clima_tempo(processed_text)
-    cidade = cidade_tempo[0]['cidade']
-    return render_template('cidade_tempo.html',
-                           cidade=cidade,
-                           desc_icon=cidade_tempo[0]['desc_icon'],
-                           temperatura=cidade_tempo[0]['temperatura'],
-                           desc_text=cidade_tempo[0]['desc_text'],
-                           umidade=cidade_tempo[0]['umidade'],
-                           visibilidade=cidade_tempo[0]['visibilidade'],
-                           **template_data)
+    try:
+        cidade = cidade_tempo[0]['cidade']
+        return render_template('cidade_tempo.html',
+                               cidade=cidade,
+                               desc_icon=cidade_tempo[0]['desc_icon'],
+                               temperatura=cidade_tempo[0]['temperatura'],
+                               desc_text=cidade_tempo[0]['desc_text'],
+                               umidade=cidade_tempo[0]['umidade'],
+                               visibilidade=cidade_tempo[0]['visibilidade'],
+                               **template_data)
+    except ValueError and TypeError:
+        return render_template('error_message.html', message='Erro ao carregar clima tempo')
 
 
 if __name__ == '__main__':
